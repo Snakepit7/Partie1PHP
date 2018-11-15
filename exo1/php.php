e10d2bfee4eacc4fc29df809e9a1d105fb541dbe
Snakepit7 <julienfollet71@gmail.com>
2018-11-15 11:09:02 +0100

    exos

---
+++ exo1/php.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Exo1</title>
+    </head>
+    <body>
+        <?php
+            $name = "Julien";
+            echo 'Salut '.$name.' !' ;
+        ?>
+    </body>
+</html>
---
+++ exo2/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Exo2</title>
+    </head>
+    <body>
+<?php
+    $lastname = "Dupond";
+    $fisrtname = "Robert";
+    $age = 70;
+    echo $lastname.' '.$fisrtname.' '.$age;
+ ?>
+    </body>
+</html>
