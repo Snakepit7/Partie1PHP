0fefd6c11459e6ff56cf910d8248e08cef6be489
Snakepit7 <julienfollet71@gmail.com>
2018-11-15 11:32:51 +0100

    exos

---
+++ exo3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo3</title>
+  </head>
+  <body>
+    <?php
+        $km = 1;
+        echo 'Nombre de km: '.$km.' ';
+        $km = 3;
+        echo 'Nombre de km: '.$km.' ';
+        $km = 125;
+        echo 'Nombre de km: '.$km;
+    ?>
+  </body>
+</html>
