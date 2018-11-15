3662ee2c928b65fb5b37cbc35a7bc26e151f9069
Snakepit7 <julienfollet71@gmail.com>
2018-11-15 11:50:58 +0100

    exos

---
+++ exo6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo6</title>
+  </head>
+  <body>
+    <?php
+        $name = "Julien";
+        echo 'Bonjour '.$name.' comment allez-vous ?';
+    ?>
+  </body>
+</html>
