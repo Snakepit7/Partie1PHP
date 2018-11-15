eba7def5a8ce06254b7378ab4b5b547ecdac2b27
Snakepit7 <julienfollet71@gmail.com>
2018-11-15 12:12:35 +0100

    exos

---
+++ exo5/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo5</title>
+  </head>
+  <body>
+    <?php
+        $varint;
+        echo $varint;
+        $varint = "42";
+        echo $varint;
+    ?>
+  </body>
+</html>
---
+++ exo7/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo7</title>
+  </head>
+  <body>
+    <?php
+        $lastname = "Dupond";
+        $firstname = "René";
+        $age = 70;
+        echo 'Bonjour '.$lastname.' '.$firstname.' tu as '.$age.' ans.';
+    ?>
+  </body>
+</html>
