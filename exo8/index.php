b2dd7c8b94f956cab22ab454fecb2560e49a191d
Snakepit7 <julienfollet71@gmail.com>
2018-11-15 12:11:14 +0100

    exos

---
+++ exo8/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo8</title>
+  </head>
+  <body>
+    <?php
+        $add = 3 + 4;
+        $multi = 5 * 20;
+        $div = 45 / 5;
+        echo 'Résultat addition: '.$add. ' ';
+        echo 'Résultat multiplication: '.$multi.' ';
+        echo 'Résultat division: '.$div.' ';
+    ?>
+  </body>
+</html>
