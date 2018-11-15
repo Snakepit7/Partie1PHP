4f6a91f07cbb30a0d054445d747d3df5e9fa8806
Snakepit7 <julienfollet71@gmail.com>
2018-11-15 11:42:43 +0100

    exo

---
+++ exo4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exo4</title>
+  </head>
+  <body>
+    <?php
+        $string = "Julien";
+        $int = 27;
+        $float = 10.5;
+        $boolean = true;
+        echo $string.' '.$int.' '.$float.' '.$boolean;
+    ?>
+  </body>
+</html>
